a16ac110f79cbf7709dfa2ffefa8568090b0e07b
KikosS41 <killian.legendre41@gmail.com>
2023-06-12 21:26:55 +0200

    Add authorization system and tested it with token wich come form identity api

--- src/routes/restorersRoutes.ts
+++ src/routes/restorersRoutes.ts
@@ -1,12 +1,14 @@
 ﻿import express from 'express';
 import * as restorersController from '../controllers/restorersController';
+import {IdentityType} from "../enums/identityType";
+import {authorize} from "../middlewares/authorizationMiddleware";
 
 const router = express.Router();
 
-router.get('/', restorersController.getAllRestorers);
-router.get('/:id', restorersController.getRestorer);
-router.post('/', restorersController.createRestorer);
-router.put('/:id', restorersController.updateRestorer);
-router.delete('/:id', restorersController.deleteRestorer);
+router.get('/',authorize([IdentityType.TECHNICAL, IdentityType.SALES]), restorersController.getAllRestorers);
+router.get('/:id', authorize([IdentityType.TECHNICAL, IdentityType.SALES, IdentityType.USER, IdentityType.RESTORER, IdentityType.DELIVERYMAN]) , restorersController.getRestorer);
+router.post('/', authorize([IdentityType.TECHNICAL, IdentityType.SALES, IdentityType.RESTORER]), restorersController.createRestorer);
+router.put('/:id', authorize([IdentityType.TECHNICAL, IdentityType.SALES, IdentityType.RESTORER]), restorersController.updateRestorer);
+router.delete('/:id', authorize([IdentityType.TECHNICAL, IdentityType.SALES, IdentityType.RESTORER]),  restorersController.deleteRestorer);
 
 export default router;
