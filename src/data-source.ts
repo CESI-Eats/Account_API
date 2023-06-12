f2302ecf27751774dbf40b090e0cb2c6c79fe09a
KikosS41 <killian.legendre41@gmail.com>
2023-06-12 22:09:40 +0200

    put db parameters in .env

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,18 +1,20 @@
-import "reflect-metadata"
-import { DataSource } from "typeorm"
-import { Restorer } from "./entity/Restorer"
-import {Address} from "./entity/Address";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { Restorer } from "./entity/Restorer";
+import { Address } from "./entity/Address";
+
+require('dotenv').config(); // Importer dotenv pour charger les variables d'environnement
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "root",
-    password: "password",
-    database: "account_dev",
+    host: process.env.DB_HOST || "localhost",
+    port: parseInt(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME || "root",
+    password: process.env.DB_PASSWORD || "password",
+    database: process.env.DB_NAME || "account_dev",
     synchronize: true,
     logging: false,
     entities: [Address, Restorer],
     migrations: [],
     subscribers: [],
-})
+});
