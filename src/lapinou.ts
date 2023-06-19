4ec45b576566bf8bf6b55314bf530d540e1e0ff6
Helrho <armand-gab@hotmail.fr>
2023-06-19 17:58:24 +0200

    [ADD] RabbitMQ to get a restorer account

--- src/lapinou.ts
+++ src/lapinou.ts
@@ -1,38 +1,38 @@
 import { Restorer } from './entity/Restorer';
-import { MessageLapinou, receiveManyMessages, sendMessage, connectLapinou } from './services/lapinouService';
+import { MessageLapinou, receiveManyMessages, sendMessage, connectLapinou, initListenTopic, handleTopic } from './services/lapinouService';
 import { AppDataSource } from './data-source'
 
 export function initLapinou(){
-    connectLapinou().then(() => {
-        receiveManyMessages('reset-restorer-kitty-payment', async (message) => {
-            console.log(`Received message: ${JSON.stringify(message)}`);
-            try {
-                const restorer = await AppDataSource.manager.findOne(Restorer, {
-                    where: {id: String((message as MessageLapinou).content)},
-                    relations: ['address']
-                });
-                restorer.kitty = 0;
-                const updatedRestorer = await AppDataSource.manager.save(restorer);
-                sendMessage({success: true, content: updatedRestorer.kitty} as MessageLapinou, 'reset-restorer-kitty-account');
-            }catch(err){
-                sendMessage({success: false, content: err} as MessageLapinou, 'reset-restorer-kitty-account');
-            }
+    connectLapinou().then(async () => {
+        const topics = ['get.restorer.account'];
+
+        initListenTopic('account', topics).then(({queue}) => {
+
+            handleTopic(queue, topics[0], async (msg) => {
+                const message = msg.content as MessageLapinou;
+                try {
+                    console.log(` [x] Received message: ${JSON.stringify(message)}`);
+                    // Check if the user already exists
+                    let restorer = await AppDataSource.manager.findOne(Restorer, {
+                        where: {id: message.content},
+                        relations: ['address']
+                    });
+            
+                    if (restorer == null) {
+                        await sendMessage({success: false, content: 'Cannot find restorer', correlationId: message.correlationId}, message.replyTo);
+                        return;
+                    }
+                    await sendMessage({success: true, content: restorer, correlationId: message.correlationId}, message.replyTo);
+                } catch (err) {
+                    const errMessage = err instanceof Error ? err.message : 'An error occurred';
+                    await sendMessage({success: false, content: errMessage, correlationId: message.correlationId}, message.replyTo);
+                }
+            });
+
+        }).catch((err) => {
+            console.error('Failed to listen to topics');
         });
-        
-        receiveManyMessages('update-restorer-kitty-command', async (message) => {
-            console.log(`Received message: ${JSON.stringify(message)}`);
-            try {
-                const restorer = await AppDataSource.manager.findOne(Restorer, {
-                    where: {id: String((message as MessageLapinou).content.restorerId)},
-                    relations: ['address']
-                });
-                restorer.kitty += (message as MessageLapinou).content.kitty;
-                const updatedRestorer = await AppDataSource.manager.save(restorer);
-                sendMessage({success: true, content: updatedRestorer.kitty} as MessageLapinou, 'update-restorer-kitty-account');
-            }catch(err){
-                sendMessage({success: false, content: err} as MessageLapinou, 'update-restorer-kitty-account');
-            }
-        });
+
     }).catch((err) => {
         console.error('Failed to connect to rabbitMQ');
     });
