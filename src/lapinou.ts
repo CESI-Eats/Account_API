09178893b04ed784d46ff65f50ebe3f594c54cd6
Helrho <armand-gab@hotmail.fr>
2023-06-19 21:07:17 +0200

    [ADD] collect-kitty handling

--- src/lapinou.ts
+++ src/lapinou.ts
@@ -1,36 +1,56 @@
 import { Restorer } from './entity/Restorer';
-import { MessageLapinou, receiveManyMessages, sendMessage, connectLapinou, initListenTopic, handleTopic } from './services/lapinouService';
+import { MessageLapinou, sendMessage, connectLapinou, handleTopic, initExchange, initQueue } from './services/lapinouService';
 import { AppDataSource } from './data-source'
+import e from 'express';
 
 export function initLapinou(){
     connectLapinou().then(async () => {
-        const topics = ['get.restorer.account'];
-
-        initListenTopic('account', topics).then(({queue}) => {
-
-            handleTopic(queue, topics[0], async (msg) => {
-                const message = msg.content as MessageLapinou;
-                try {
-                    console.log(` [x] Received message: ${JSON.stringify(message)}`);
-                    // Check if the user already exists
-                    let restorer = await AppDataSource.manager.findOne(Restorer, {
-                        where: {id: message.content},
-                        relations: ['address']
-                    });
-            
-                    if (restorer == null) {
-                        await sendMessage({success: false, content: 'Cannot find restorer', correlationId: message.correlationId}, message.replyTo);
-                        return;
+        initExchange('restorers').then(exchange => {
+            initQueue(exchange, 'get.restorer.account').then(({queue, topic}) => {
+                handleTopic(queue, topic, async (msg) => {
+                    const message = msg.content as MessageLapinou;
+                    try {
+                        console.log(` [x] Received message: ${JSON.stringify(message)}`);
+                        // Check if the user already exists
+                        let restorer = await AppDataSource.manager.findOne(Restorer, {
+                            where: {id: message.content.id},
+                            relations: ['address']
+                        });
+                
+                        if (restorer == null) {
+                            await sendMessage({success: false, content: 'Cannot find restorer', correlationId: message.correlationId}, message.replyTo);
+                            return;
+                        }
+                        await sendMessage({success: true, content: restorer, correlationId: message.correlationId}, message.replyTo);
+                    } catch (err) {
+                        const errMessage = err instanceof Error ? err.message : 'An error occurred';
+                        await sendMessage({success: false, content: errMessage, correlationId: message.correlationId, sender: 'account'}, message.replyTo);
                     }
-                    await sendMessage({success: true, content: restorer, correlationId: message.correlationId}, message.replyTo);
-                } catch (err) {
-                    const errMessage = err instanceof Error ? err.message : 'An error occurred';
-                    await sendMessage({success: false, content: errMessage, correlationId: message.correlationId}, message.replyTo);
-                }
+                });
             });
-
-        }).catch((err) => {
-            console.error('Failed to listen to topics');
+            initQueue(exchange, 'collect.restorer.kitty').then(({queue, topic}) => {
+                handleTopic(queue, topic, async (msg) => {
+                    const message = msg.content as MessageLapinou;
+                    try {
+                        console.log(` [x] Received message: ${JSON.stringify(message)}`);
+                        // Check if the user already exists
+                        let restorer = await AppDataSource.manager.findOne(Restorer, {
+                            where: {id: message.content.id},
+                            relations: ['address']
+                        });
+                
+                        if (restorer == null) {
+                            throw new Error('Cannot find restorer');
+                        }
+                        restorer.kitty = 0;
+                        await AppDataSource.manager.save(restorer);
+                        await sendMessage({success: true, content: {kitty: restorer.kitty}, correlationId: message.correlationId, sender: 'account'}, message.replyTo);
+                    } catch (err) {
+                        const errMessage = err instanceof Error ? err.message : 'An error occurred';
+                        await sendMessage({success: false, content: errMessage, correlationId: message.correlationId, sender: 'account'}, message.replyTo);
+                    }
+                });
+            });
         });
 
     }).catch((err) => {
