bfe20314693ff6d95866fb520587c5b23d149888
KikosS41 <killian.legendre41@gmail.com>
2023-06-20 20:05:36 +0200

    correct error armand saw

--- src/lapinou.ts
+++ src/lapinou.ts
@@ -1,11 +1,13 @@
 import { createRestorerExchange } from './exchanges/restorerExchange';
 import { connectLapinou } from './services/lapinouService';
 import {createUserExchange} from "./exchanges/userExchange";
+import {createDeliveryManExchange} from "./exchanges/deliverymanExchange";
 
 export function initLapinou(){
     connectLapinou().then(async () => {
         createUserExchange();
         createRestorerExchange();
+        createDeliveryManExchange();
     }).catch((err) => {
         console.error('Failed to connect to rabbitMQ');
     });
