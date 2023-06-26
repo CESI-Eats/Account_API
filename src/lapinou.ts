1d4ac0a280716449c7ab0c86f735d4e3be3da38b
KikosS41 <killian.legendre41@gmail.com>
2023-06-26 15:40:42 +0200

    fusionned

--- src/lapinou.ts
+++ src/lapinou.ts
@@ -3,6 +3,7 @@ import { connectLapinou } from './services/lapinouService';
 import {createUserExchange} from "./exchanges/userExchange";
 import {createDeliveryManExchange} from "./exchanges/deliverymanExchange";
 import { createOrderingExchange } from './exchanges/orderingExchange';
+import {createHistoricExchange} from "./exchanges/historicExchange";
 
 export function initLapinou(){
     connectLapinou().then(async () => {
@@ -10,6 +11,7 @@ export function initLapinou(){
         createRestorerExchange();
         createDeliveryManExchange();
         createOrderingExchange();
+        createHistoricExchange();
     }).catch((err) => {
         console.error('Failed to connect to rabbitMQ');
     });
