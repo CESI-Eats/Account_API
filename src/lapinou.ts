f645d5cc85c5fd305a027bc8ae17b42ed7bca900
KikosS41 <killian.legendre41@gmail.com>
2023-06-27 14:24:20 +0200

    ordersExchange

--- src/lapinou.ts
+++ src/lapinou.ts
@@ -1,17 +1,19 @@
-import { createRestorerExchange } from './exchanges/restorerExchange';
-import { connectLapinou } from './services/lapinouService';
+import {createRestorerExchange} from './exchanges/restorerExchange';
+import {connectLapinou} from './services/lapinouService';
 import {createUserExchange} from "./exchanges/userExchange";
 import {createDeliveryManExchange} from "./exchanges/deliverymanExchange";
-import { createOrderingExchange } from './exchanges/orderingExchange';
+import {createOrderingExchange} from './exchanges/orderingExchange';
 import {createHistoricExchange} from "./exchanges/historicExchange";
+import {createOrdersExchange} from "./exchanges/ordersExchange";
 
-export function initLapinou(){
+export function initLapinou() {
     connectLapinou().then(async () => {
         createUserExchange();
         createRestorerExchange();
         createDeliveryManExchange();
         createOrderingExchange();
         createHistoricExchange();
+        createOrdersExchange();
     }).catch((err) => {
         console.error('Failed to connect to rabbitMQ');
     });
